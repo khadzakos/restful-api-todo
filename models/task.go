1fa368cc212a0352e7fa61ef0512ae915950f8a9
Nikolay Khadzakos <hadzakos@gmail.com>
2024-09-05 18:16:07 +0300

    add constructor and methods update & complete

--- models/task.go
+++ models/task.go
@@ -10,3 +10,25 @@ type Task struct {
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
+
+func NewTask(title, description string) *Task {
+	return &Task{
+		Title:       title,
+		Description: description,
+		Status:      "pending", // default status
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+}
+
+func (t *Task) Update(title, description, status string) {
+	t.Title = title
+	t.Description = description
+	t.Status = status
+	t.UpdatedAt = time.Now()
+}
+
+func (t *Task) Complete() {
+	t.Status = "completed"
+	t.UpdatedAt = time.Now()
+}
